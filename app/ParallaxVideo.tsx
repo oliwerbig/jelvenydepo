583b15ca2f3c05ea82291f852df0f676c389d072
oliwerbig <oliver.emanuel.nagy@gmail.com>
2023-08-07 15:45:28 +0200

    -

--- app/ParallaxVideo.tsx
+++ app/ParallaxVideo.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useParallax } from "react-scroll-parallax";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 interface Props {
   zIndex?: number;
@@ -13,22 +14,36 @@ const ParallaxVideo = ({ zIndex, className, ...rest }: Props) => {
     speed: -100000,
   });
 
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      loop: 1,
+      muted: 1,
+      controls: 0,
+    },
+  };
+
   return (
     <div
       {...rest}
       ref={parallax.ref as React.RefObject<HTMLDivElement>}
       className={`relative -z-10 z-[${zIndex}] -mb-[100vh] flex h-screen items-center justify-center overflow-hidden ${className}`}
     >
-      <video
-        className={`absolute min-h-full w-auto min-w-full max-w-none`}
-        controls
-        autoPlay
-        muted
-        preload="auto"
-        loop
-      >
-        <source src="/media/jelvenydepo/muhely_cut.mp4" type="video/mp4" />
-      </video>
+      <div className={`absolute min-h-full w-auto min-w-full max-w-none`}>
+        <YouTube
+          videoId="h-BzKPqOv9g"
+          opts={opts}
+          onReady={onPlayerReady}
+        ></YouTube>
+      </div>
     </div>
   );
 };
